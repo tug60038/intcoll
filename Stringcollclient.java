48680603a7ef778edf3d6156da55f16359131929
Joey Norkin <joey.norkin@gmail.com>
2015-03-10 13:14:31 -0400

    updated SENTINEL

--- Stringcollclient.java
+++ Stringcollclient.java
@@ -6,14 +6,14 @@
 import java.util.*;
 
 public class Stringcollclient {
-    public static final String SENTINEL = "/EXIT";
+    public static final String SENTINEL = "EXIT";
     
     public static void main(String[] args) {
         String value; char sign;
         Scanner keyboard = new Scanner(System.in);
         Stringcoll P = new Stringcoll(), N = new Stringcoll(), L = new Stringcoll();
         
-        System.out.println("Enter a String to be inserted or enter the command, '/EXIT', to quit:");
+        System.out.println("Enter a String to be inserted or enter the command, 'EXIT', to quit:");
         
         value = keyboard.next();
         while (!value.equals(SENTINEL)) {
@@ -28,7 +28,7 @@ public class Stringcollclient {
             } else {
                 System.out.println("Invalid entry.");
             }
-            System.out.println("Enter next String to be inserted or enter the command '/EXIT' to quit:");
+            System.out.println("Enter next String to be inserted or enter the command 'EXIT' to quit:");
             value = keyboard.next();
         }
         System.out.println("\nThe values in collection P are:");
